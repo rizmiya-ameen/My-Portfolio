2f5c6317cccfb54dbda4c0d434f549dfd419a239
rizmiya-ameen <rizmiyaamin@gmail.com>
2024-02-08 12:49:17 +1100

    added nodemailer

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,18 +1,53 @@
-"use client"
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 import { useSectionInView } from "@/lib/hooks";
-import SectionHeading from './SectionHeading';
+import SectionHeading from "./SectionHeading";
 import { motion } from "framer-motion";
-//import toast from 'react-hot-toast';
+import SubmitBtn from "./Submit-btn";
+import toast from "react-hot-toast";
 
 const Contact = () => {
-
   const { ref } = useSectionInView("Contact");
 
+  const [loading, setLoading] = useState(false);
+
+  // const handleSubmit = async (event: any) => {
+  //   event.preventDefault();
+  //   setLoading(true);
+
+  //   const data = {
+  //     name: String(event.target.name.value),
+  //     email: String(event.target.email.value),
+  //     message: String(event.target.message.value),
+  //   };
+
+  //   console.log(data);
+
+  //   const response = await fetch("/api/contact", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(data),
+  //   });
+
+  //   if (response.ok) {
+  //     console.log("Message sent successfully");
+  //     setLoading(false);
+  //     // reset the form
+  //     event.target.name.value = "";
+  //     event.target.email.value = "";
+  //     event.target.message.value = "";
+  //   }
+  //   if (!response.ok) {
+  //     console.log("Error sending message");
+  //     setLoading(false);
+  //   }
+  // };
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
-    //setLoading(true);
+    setLoading(true);
 
     const data = {
       name: String(event.target.name.value),
@@ -20,35 +55,37 @@ const Contact = () => {
       message: String(event.target.message.value),
     };
 
-    console.log(data);
+    try {
+      const response = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-    const response = await fetch("/api/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (response.ok) {
-      console.log("Message sent successfully");
-      //setLoading(false);
-      // reset the form
-      // event.target.name.value = "";
-      // event.target.email.value = "";
-      // event.target.message.value = "";
-    }
-    if (!response.ok) {
-      console.log("Error sending message");
-      //setLoading(false);
+      if (response.ok) {
+        console.log("Message sent successfully");
+        toast.success("Message sent successfully!"); // Display success toast
+        setLoading(false);
+        // reset the form
+        event.target.name.value = "";
+        event.target.email.value = "";
+        event.target.message.value = "";
+      } else {
+        throw new Error("Error sending message");
+      }
+    } catch (error) {
+      console.error("Error sending message:", error);
+      toast.error("Error sending message!"); // Display error toast
+      setLoading(false);
     }
   };
 
-
   return (
-    <motion.section 
-      id="contact" 
-      ref={ref} 
+    <motion.section
+      id="contact"
+      ref={ref}
       className="flex flex-col w-full leading-7 px-5 pt-28 pb-16 min-h-[100vh] text-center bg-gradient-to-r from-indigo-500 to-cyan-500"
       initial={{
         opacity: 0,
@@ -63,11 +100,10 @@ const Contact = () => {
         once: true,
       }}
     >
-
       <div className="relative text-white">
         <SectionHeading>Get in Touch</SectionHeading>
         <h1 className="hidden sm:inline sm:tracking-wide sm:absolute sm:top-0 sm:bottom-0 sm:right-0 sm:left-0 sm:text-[90px] sm:font-extrabold sm:opacity-[0.1] sm:text-white">
-        CONTACT
+          CONTACT
         </h1>
       </div>
 
@@ -80,36 +116,27 @@ const Contact = () => {
       </p>
 
       <form
-      onSubmit={handleSubmit}
+        onSubmit={handleSubmit}
         className="mt-6 flex flex-col mx-auto w-[min(100%,38rem)]"
-        /*action={async (formData) => {
-          await sendEmail(formData)
-        }}*/
-        // action={async (formData) => {
-        //   const { data, error } = await sendEmail(formData);
-
-        //   if (error) {
-        //     toast.error(error);
-        //    return;
-        //   }
-
-        //   toast.success("Email sent successfully!")
-        // }}
       >
-      
         <input
           type="text"
           minLength={3}
           maxLength={150}
           required
-          className="h-14 my-3 px-4 rounded-lg borderBlack"
+          className={`h-14 my-3 px-4 rounded-lg borderBlack ${
+            loading && "text-gray-300 cursor-default"
+          }`}
           autoComplete="off"
           id="name"
           placeholder="Your Name"
+          readOnly={loading} // Disable editing while loading
         />
 
         <input
-          className="h-14  px-4 rounded-lg borderBlack"
+          className={`h-14  px-4 rounded-lg borderBlack ${
+            loading && "text-gray-300 cursor-default"
+          }`}
           type="email"
           required
           minLength={5}
@@ -117,25 +144,168 @@ const Contact = () => {
           placeholder="Your email"
           autoComplete="off"
           id="email"
+          readOnly={loading}
         />
 
         <textarea
-          className="h-52 my-3 rounded-lg borderBlack p-4"
+          className={`h-52 my-3 rounded-lg borderBlack p-4 ${
+            loading && "text-gray-300 cursor-default"
+          } `}
           name="message"
           placeholder="Your message"
           required
+          minLength={10}
           maxLength={5000}
-          //rows={4}
+          readOnly={loading}
         />
-        <div className='flex justify-center'>
-          <button type="submit">Submit</button>
+
+
+        <div className="flex justify-center">
+        <SubmitBtn loading={loading} />
         </div>
-        
       </form>
-
-
     </motion.section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
+
+// "use client"
+// import React from 'react'
+// import { useSectionInView } from "@/lib/hooks";
+// import SectionHeading from './SectionHeading';
+// import { motion } from "framer-motion";
+// import SubmitBtn from './Submit-btn';
+// //import toast from 'react-hot-toast';
+
+// const Contact = () => {
+
+//   const { ref } = useSectionInView("Contact");
+
+//   const handleSubmit = async (event: any) => {
+//     event.preventDefault();
+//     //setLoading(true);
+
+//     const data = {
+//       name: String(event.target.name.value),
+//       email: String(event.target.email.value),
+//       message: String(event.target.message.value),
+//     };
+
+//     console.log(data);
+
+//     const response = await fetch("/api/contact", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(data),
+//     });
+
+//     if (response.ok) {
+//       console.log("Message sent successfully");
+//       //setLoading(false);
+//       // reset the form
+//       // event.target.name.value = "";
+//       // event.target.email.value = "";
+//       // event.target.message.value = "";
+//     }
+//     if (!response.ok) {
+//       console.log("Error sending message");
+//       //setLoading(false);
+//     }
+//   };
+
+//   return (
+//     <motion.section
+//       id="contact"
+//       ref={ref}
+//       className="flex flex-col w-full leading-7 px-5 pt-28 pb-16 min-h-[100vh] text-center bg-gradient-to-r from-indigo-500 to-cyan-500"
+//       initial={{
+//         opacity: 0,
+//       }}
+//       whileInView={{
+//         opacity: 1,
+//       }}
+//       transition={{
+//         duration: 1,
+//       }}
+//       viewport={{
+//         once: true,
+//       }}
+//     >
+
+//       <div className="relative text-white">
+//         <SectionHeading>Get in Touch</SectionHeading>
+//         <h1 className="hidden sm:inline sm:tracking-wide sm:absolute sm:top-0 sm:bottom-0 sm:right-0 sm:left-0 sm:text-[90px] sm:font-extrabold sm:opacity-[0.1] sm:text-white">
+//         CONTACT
+//         </h1>
+//       </div>
+
+//       <p className="text-gray-300 mt-5">
+//         Please contact me directly at{" "}
+//         <a className="underline" href="mailto:rizmiyaamin@gmail.com">
+//           rizmiyaamin@gmail.com
+//         </a>{" "}
+//         or through this form.
+//       </p>
+
+//       <form
+//       onSubmit={handleSubmit}
+//         className="mt-6 flex flex-col mx-auto w-[min(100%,38rem)]"
+//         /*action={async (formData) => {
+//           await sendEmail(formData)
+//         }}*/
+//         // action={async (formData) => {
+//         //   const { data, error } = await sendEmail(formData);
+
+//         //   if (error) {
+//         //     toast.error(error);
+//         //    return;
+//         //   }
+
+//         //   toast.success("Email sent successfully!")
+//         // }}
+//         >
+
+//         <input
+//           type="text"
+//           minLength={3}
+//           maxLength={150}
+//           required
+//           className="h-14 my-3 px-4 rounded-lg borderBlack"
+//           autoComplete="off"
+//           id="name"
+//           placeholder="Your Name"
+//         />
+
+//         <input
+//           className="h-14  px-4 rounded-lg borderBlack"
+//           type="email"
+//           required
+//           minLength={5}
+//           maxLength={150}
+//           placeholder="Your email"
+//           autoComplete="off"
+//           id="email"
+//         />
+
+//         <textarea
+//           className="h-52 my-3 rounded-lg borderBlack p-4"
+//           name="message"
+//           placeholder="Your message"
+//           required
+//           maxLength={5000}
+//           //rows={4}
+//         />
+//          <div className='flex justify-center'>
+//           <button type="submit">Submit</button>
+//         </div>
+
+//       </form>
+
+//     </motion.section>
+//   )
+// }
+
+// export default Contact
--- components/Submit-btn.tsx
+++ components/Submit-btn.tsx
@@ -1,3 +1,32 @@
+import React from "react";
+import { FaPaperPlane } from "react-icons/fa";
+
+
+export default function SubmitBtn({loading}: any) {
+  
+
+  return (
+    <button
+      type="submit"
+      className="group mt-2 flex items-center justify-center gap-4 h-[3rem] w-[8rem] bg-gray-800 text-white rounded-md outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105  disabled:scale-100 disabled:bg-opacity-65"
+      disabled={loading}
+    >
+      {loading ? (
+        <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>
+      ) : (
+        <>
+          Submit{" "}
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
+        </>
+      )}
+    </button>
+  );
+}
+
+
+
+
+
 // import React from "react";
 // import { FaPaperPlane } from "react-icons/fa";
 // import { experimental_useFormStatus as useFormStatus } from "react-dom";
