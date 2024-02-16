7651053f930d5231e4da9377895cced2c2a1a88a
rizmiya-ameen <rizmiyaamin@gmail.com>
2024-02-16 17:50:40 +1100

    new fonts added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,54 +1,54 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter, Croissant_One, Nunito, Dancing_Script} from 'next/font/google'
-import ActiveSectionContextProvider from '@/context/active-section.context'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter, Croissant_One, Nunito, Dancing_Script } from "next/font/google";
+import ActiveSectionContextProvider from "@/context/active-section.context";
 import { Toaster } from "react-hot-toast";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-const nunito = Nunito({ 
-  subsets: ['latin'] ,
+const nunito = Nunito({
+  subsets: ["latin"],
   display: "swap",
-  variable: '--font-Nunito',
-  weight: "200"
-})
+  variable: "--font-Nunito",
+  weight: "200",
+});
 
-const croissant = Croissant_One({ 
-  subsets: ['latin'] ,
+const croissant = Croissant_One({
+  subsets: ["latin"],
   display: "swap",
-  variable: '--font-Croissant_One',
-  weight: "400"
-})
+  variable: "--font-Croissant_One",
+  weight: "400",
+});
 
-const dancing = Dancing_Script({ 
-  subsets: ['latin'] ,
+const dancing = Dancing_Script({
+  subsets: ["latin"],
   display: "swap",
-  variable: '--font-Dancing_Script',
-  weight: "700"
-})
+  variable: "--font-Dancing_Script",
+  weight: "700",
+});
 
 export const metadata: Metadata = {
-  title: 'Rizmiya | Portfolio',
-  description: 'Passionate React learner building meaningful and intuitive web interfaces. This portfolio reflects my enthusiasm for crafting delightful user experiences and my commitment to continuous improvement. Eager to apply my skills and contribute to projects that make a difference.'
-}
+  title: "Rizmiya | Portfolio",
+  description:
+    "Passionate React learner building meaningful and intuitive web interfaces. This portfolio reflects my enthusiasm for crafting delightful user experiences and my commitment to continuous improvement. Eager to apply my skills and contribute to projects that make a difference.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`!scroll-smooth ${nunito.variable} ${croissant.variable} ${dancing.variable}`}>
-
+    <html
+      lang="en"
+      className={`!scroll-smooth ${nunito.variable} ${croissant.variable} ${dancing.variable}`}
+    >
       <body className={`${inter.className} bg-slate-50 text-gray-950 relative`}>
-      
         <ActiveSectionContextProvider>
           {children}
           <Toaster position="top-center" />
         </ActiveSectionContextProvider>
-        
       </body>
     </html>
-  )
+  );
 }
-
